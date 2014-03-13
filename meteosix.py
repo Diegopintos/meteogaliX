80297359b80eef5677189eec6e6caa6c2b500f47
jose <joseliki@debian.joseliki>
2014-03-13 20:00:16 +0100

    tarea puertos

--- meteosix.py
+++ meteosix.py
@@ -1,19 +1,49 @@
 #!/usr/bin/python
-#*-* coding: UTF-8 *-*
+# *-* coding: UTF-8 *-*
 import requests
-from jinja2 import Template
-import json
 from lxml import etree
+import webbrowser
 
+print """
+1 	A Coruña
+3 	Vigo
+4 	Villagarcía
+5 	Ferrol
+6 	Ría de Foz
+7 	Corcubión
+8 	Ría de Camarinas
+9 	Ría de Corme
+10	 A Guarda
+11	 Ribeira
+12	 Muros
+13	 Pontevedra 
+	"""
 
-entrada = raw_input("introduce tu api key:\t")
+file = open('salida.html','w')
+provincia = int(raw_input("de que puerto, quieres saber la información de las mareas?:\n"))
+puertos = {1:'A coruña',3:'Vigo',4:'Villagarcía',5:'Ferrol',6:'Ría de Foz'
+,7:'Corcubión',8:'Ría de Camariñas',9:'Ría de Corme',10:'A guarda',11:'Ribeira',12:'Muros',13:'Pontevedra'}
+api = raw_input("introduce tu api key:\n")
 url = 'http://servizos.meteogalicia.es/apiv2/getTidesInfo' 
-valores = {'coord':'-8.637,43.45','API_KEY':'%s'}
-valores['API_KEY'] = entrada
+valores = {'coord':'-8.637,43.45','format':'kml','API_KEY':api}
 response = requests.get(url, params=valores)
-dato = json.loads(response.text.encode('UTF-8'))
-lista_puertos = []
-lista.puertos.append(dato)
+dato = etree.fromstring(response.text.encode('utf-8'))
+salida = etree.tostring(dato)
+for element in salida.iter(tag='state'):
+	print element.tag
+
+file.write(salida)
+
+
+	
+
+
+	
+	
+
+
+
+
 
 	
 
